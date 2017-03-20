49e77d45c62d4b647ff53807094d9b86a2b4c63a
Ryan Parrish <ryan@stickystyle.net>
2017-03-20 15:10:05 -0400

    redirect output

--- bin/get_pending_commits_sensor.sh
+++ bin/get_pending_commits_sensor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd "/home/homeassistant/.homeassistant" > /dev/null
-git fetch > /dev/null
+cd "/home/homeassistant/.homeassistant" > /dev/null 2>&1
+git fetch > /dev/null 2>&1
 git rev-list --count master..origin/master
